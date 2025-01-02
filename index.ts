d34f29dc5c1d26ecd8a0fead8ee602d86d3cfe6b
nikhilBornare <nikhilbornare01@gmail.com>
2025-01-02 23:50:43 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import connectDB from './src/config/db';
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Basic Route
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running...');
+});
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
