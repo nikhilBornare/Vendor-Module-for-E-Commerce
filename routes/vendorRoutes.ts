64583508e7eff52a6ad3b30daf8aa473c230b131
nikhilBornare <nikhilbornare01@gmail.com>
2025-01-03 17:39:02 +0530

    error fixed for updateMultiple and DeleteMultiple

--- routes/vendorRoutes.ts
+++ routes/vendorRoutes.ts
@@ -1,14 +1,29 @@
 import express from "express";
-import { createMultipleVendors, createVendor, deleteMultipleVendors, deleteVendor, getAllVendors, getVendorById, updateMultipleVendors, updateVendor } from "../controllers/vendorController";
-
+import {
+  createMultipleVendors,
+  createVendor,
+  deleteMultipleVendors,
+  deleteVendor,
+  getAllVendors,
+  getVendorById,
+  updateMultipleVendors,
+  updateVendor,
+} from "../controllers/vendorController";
 
 const router = express.Router();
 
-// Route to create vendor
-router.post("/" , createVendor);
-
 // Route to create multiple vendors
-router.post("/bulk",createMultipleVendors);
+router.post("/bulk", createMultipleVendors);
+
+// Route to update multiple vendors
+router.put("/bulk", updateMultipleVendors);
+
+// Route to delete multiple vendors by ID
+router.delete("/", deleteMultipleVendors);
+
+// Generic routes come after specific routes
+// Route to create a vendor
+router.post("/", createVendor);
 
 // Route to get all vendors
 router.get("/", getAllVendors);
@@ -17,15 +32,9 @@ router.get("/", getAllVendors);
 router.get("/:id", getVendorById);
 
 // Route to update a vendor by ID
-router.put("/:id",updateVendor);
-
-// Route to update multiple Vendors 
-router.put("/bulk/", updateMultipleVendors);
+router.put("/:id", updateVendor);
 
 // Route to delete a vendor by ID
 router.delete("/:id", deleteVendor);
-
-// Route to delete multiple vendors by ID
-router.delete("/",deleteMultipleVendors);
 
 export default router;
